3cfd3e402c03b81a8710a58dfe5d7438e965b347
Shubh Kapoor <85066072+shubhkapoor@users.noreply.github.com>
2023-12-20 16:11:30 +0530

    Minimum Multiplications to reach End.cpp

---
+++ Graph/Minimum Multiplications to reach End.cpp
@@ -0,0 +1,52 @@
+/*
+
+https://www.geeksforgeeks.org/problems/minimum-multiplications-to-reach-end/1
+
+*/
+
+class Solution {
+  public:
+    int minimumMultiplications(vector<int>& arr, int start, int end) {
+        // code here
+        
+        if(start == end)
+        {
+            return 0;
+        }
+        
+        // {node , steps}
+        queue<pair<int,int>> q;
+        vector<int> dist(100000 , 1e9);
+        
+        dist[start] = 0;
+        q.push({start,0});
+        
+        int mod = 100000;
+        
+        while(!q.empty())
+        {
+            int node = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+            
+            for(auto it : arr)
+            {
+                int num = (it * node)%mod;
+                
+                if(steps+1 < dist[num])
+                {
+                    dist[num] = steps+1;
+                    
+                    if(num == end)
+                    {
+                        return steps+1;
+                    }
+                    
+                    q.push({num , steps+1});
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
