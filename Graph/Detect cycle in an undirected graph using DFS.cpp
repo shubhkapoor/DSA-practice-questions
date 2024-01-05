5aca0fc572cfd8c20ac61bc05dafdd8e47a5d4ee
Shubh Kapoor <85066072+shubhkapoor@users.noreply.github.com>
2024-01-05 13:37:14 +0530

    Detect cycle in an undirected graph using DFS.cpp

--- Graph/Detect cycle in an undirected graph using DFS.cpp
+++ Graph/Detect cycle in an undirected graph using DFS.cpp
@@ -1,28 +1,27 @@
 /*
 
-https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=detect-cycle-in-an-undirected-graph
+https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
 
 */
 
 class Solution {
-  private:
+  public:
+    // Function to detect cycle in an undirected graph.
     
-    bool detectCycle(int node , int parent , int vis[] , vector<int> adj[])
+    bool dfscheck(int node , int parent , int vis[] , vector<int> adj[])
     {
         vis[node] = 1;
         
-        for(auto adjnode : adj[node])
+        for(auto it : adj[node])
         {
-            if(!vis[adjnode])
+            if(!vis[it])
             {
-                vis[adjnode] = 1;
-                
-                if(detectCycle(adjnode , node , vis , adj))
+                if(dfscheck(it , node , vis , adj))
                 {
                     return true;
                 }
             }
-            else if(adjnode != parent)
+            else if(it != parent)
             {
                 return true;
             }
@@ -31,18 +30,17 @@ class Solution {
         return false;
     }
     
-  public:
-    // Function to detect cycle in an undirected graph.
+    
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
         
         int vis[V] = {0};
         
-        for(int i=0 ; i<V; i++)
+        for(int i=0 ; i<V ; i++)
         {
             if(!vis[i])
             {
-                if(detectCycle(i , -1 , vis , adj))
+                if(dfscheck(i , -1 , vis , adj))
                 {
                     return true;
                 }
